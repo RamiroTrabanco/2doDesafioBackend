67205f734e12165ecf3099f960a1696dac8de9d9
RamiroTrabanco <trabancoramiro@gmail.com>
2023-01-17 11:41:38 -0300

    2do commit

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ class ProductManager {
     }
 
     async addProducts(title, description, price, thumbnail, code, stock){
+
+        try {
+        
+        const prodsFile = await this.getProducts()
+        const productCode = this.#validateCodeProduct(code)
+
         const product = {
             title,
             description,
@@ -18,17 +24,11 @@ class ProductManager {
             id: this.#generateId()
         }
 
-        try {
-
-        const productCode = this.#validateCodeProduct(code)
-        console.log(code)
-        console.log(productCode)
-
         if(productCode === undefined && title && description && price && thumbnail && stock){
-            const prodsFile = await this.getProducts()
+
             prodsFile.push(product)
-            await fs.promises.writeFile(this.path, JSON.stringify(prodsFile))
-        }}
+            await fs.promises.writeFile(this.path, JSON.stringify(prodsFile))}}
+
         catch(error){
             console.log("error")
         }
@@ -36,18 +36,17 @@ class ProductManager {
     }
 
     #validateCodeProduct(code) {
-        return this.products.find(product=>product.code===code)
+        return this.products.find(prod=>prod.code===code)
     }
 
     async getProducts(){
         try {
             if(fs.existsSync(this.path)){
-                const prods = await fs.promises.readFile(this.path, "utf-8")
-                const prodsJS = JSON.parse(prods)
-                this.products.push(prodsJS)
-                return prodsJS 
+                const products = await fs.promises.readFile(this.path, "utf-8")
+                this.products = JSON.parse(products)
+                return this.products
             } else {
-                return []
+                return this.products = []
             }
         }
         catch(error) {
@@ -57,7 +56,7 @@ class ProductManager {
     
     async getProductsById(id){
         const prodsFileId = await this.getProducts()
-        const productById = await prodsFileId.find((product)=>product.id===id)
+        const productById = prodsFileId.find(product=>product.id===id)
         if (productById === undefined){
             console.log("Not found")
         } else {
@@ -66,14 +65,25 @@ class ProductManager {
     }
 
     async updateProduct(prod){
-        console.log(prod)
         let updateProd = await this.getProductsById(prod.id)
-        if(updateProd !== prod && updateProd.id == prod.id){
-            prod = updateProd
-            return this.addProducts(prod)
+        const productCode = this.#validateCodeProduct(prod.code)
+        if(updateProd !== prod && updateProd.id == prod.id && productCode === undefined){
+            this.products.push(prod)
+            this.deleteProduct(updateProd.id)
+            console.log(this.products)
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+            return this.products
         } else {
             console.log("El producto no se pudo actualizar")
         }
+    }
+    
+
+    async deleteProduct(prodId){
+        const deleteProd = await this.getProducts()
+        deleteProd.splice(prodId - 1, 1)
+        await fs.promises.writeFile(this.path, JSON.stringify(deleteProd))
+        return deleteProd
     }
 
     #generateId() {
@@ -87,16 +97,14 @@ class ProductManager {
 
 
 /* PRUEBA
-
-let product = new ProductManager()
-
-product.addProducts("Manzana", "Red", 10, "https://images.emojiterra.com/google/android-pie/512px/1f34e.png", 50, 100)
-product.addProducts("Naranja", "Orange", 10, "https://images.emojiterra.com/google/android-pie/512px/1f34e.png", 500, 100)
-product.addProducts("Naranja", 10, "https://images.emojiterra.com/google/android-pie/512px/1f34e.png", 50, 100)
 console.log(product.getProducts())
 console.log(product.getProductsById(1))
 console.log(product.getProductsById(5))
 console.log(product.getProductsById(1))
+ */
+
+/* let product = new ProductManager()
+
 let cereza = {
     title: "Cereza",
     description: "Roja",
@@ -106,9 +114,8 @@ let cereza = {
     stock: 5000,
     id: 1
 } 
-product.updateProduct(cereza)
+product.updateProduct(cereza) */
 
-
-arreglar no poder agregar dos productos distintos
-arreglar updateProduct
-agregar/arreglar deleteProduct */
+ 
+/* product.addProducts("Manzana", "Red", 10, "https://images.emojiterra.com/google/android-pie/512px/1f34e.png", 50, 100)
+product.addProducts("Naranja", "Orange", 10, "https://images.emojiterra.com/google/android-pie/512px/1f34e.png", 500, 100) */
